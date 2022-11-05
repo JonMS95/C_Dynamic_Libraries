56697c875015a40ffcbe23d744e9f5a9bf88663e
Jon Ubuntu <jon_portugalete@hotmail.com>
2022-11-05 19:47:20 +0100

    Added main.h file, where include and define statements (that were initially within main.c file) are stored.

--- Source_files/main.c
+++ Source_files/main.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <dlfcn.h>
-
-#define CORRECT_ARGC_VALUE      3
-#define ARGUMENT_NUMBER_MESSAGE "Only two arguments should be passed on the command line!\r\n"
-#define NON_ZERO_MESSAGE        "Both arguments should be non-zero integers!\r\n"
-
-#define MULT_SO_PATH    "/home/jon/Desktop/scripts/C/C_Dynamic_Libraries_from_executable/Dynamic_libraries/mult.so"
-#define DIV_SO_PATH     "/home/jon/Desktop/scripts/C/C_Dynamic_Libraries_from_executable/Dynamic_libraries/div.so"
+#include "main.h"
 
 /*
 @brief Takes the two numbers that have been passed by command line and the path
---
+++ Source_files/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+#define CORRECT_ARGC_VALUE      3
+#define ARGUMENT_NUMBER_MESSAGE "Only two arguments should be passed on the command line!\r\n"
+#define NON_ZERO_MESSAGE        "Both arguments should be non-zero integers!\r\n"
+
+#define MULT_SO_PATH    "/home/jon/Desktop/scripts/C/C_Dynamic_Libraries_from_executable/Dynamic_libraries/mult.so"
+#define DIV_SO_PATH     "/home/jon/Desktop/scripts/C/C_Dynamic_Libraries_from_executable/Dynamic_libraries/div.so"
+
+#endif
